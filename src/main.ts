d799eb48945938f51b904f79626ddbeae63d594b
RedInJapanese <abasu924@gmail.com>
2024-10-07 15:06:26 -0700

    complete step 9

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+}
+
+const availableItems: Item[] = [
+  { name: "Bird Tier 1", cost: 10, rate: 0.1 },
+  { name: "Bird Tier 2", cost: 100, rate: 2 },
+  { name: "Bird Tier 3", cost: 1000, rate: 50 },
+];
+
 const gameName = "Bird Game";
 document.title = gameName;
 
@@ -15,51 +27,48 @@ app.append(button);
 
 let x = 0;
 let growth_rate = 0;
-let b2_price = 10;
-let b3_price = 100;
-let b4_price = 1000;
 
 const count = document.createElement("h2");
 count.innerHTML = "Bird count: ";
 app.append(count);
 
 const button2 = document.createElement("button");
-button2.innerHTML = "Buy 0.1 Bird(s)";
+button2.innerHTML = availableItems[0].name;
 app.append(button2);
 
 const cost = document.createElement("h2");
-cost.innerHTML = "Cost: 10 Bird(s)";
+cost.innerHTML = "Cost: " + availableItems[0].cost + " Bird(s)";
 app.append(cost);
 
 const button3 = document.createElement("button");
-button3.innerHTML = "Buy 2.0 Bird(s)";
+button3.innerHTML = availableItems[1].name;
 app.append(button3);
 
 const cost2 = document.createElement("h2");
-cost2.innerHTML = "Cost: 100 Bird(s)";
+cost2.innerHTML = "Cost: " + availableItems[1].cost + " Bird(s)";
 app.append(cost2);
 
 const button4 = document.createElement("button");
-button4.innerHTML = "Buy 50 Bird(s)";
+button4.innerHTML = availableItems[2].name;
 app.append(button4);
 
 const cost3 = document.createElement("h2");
-cost3.innerHTML = "Cost: 1000 Bird(s)";
+cost3.innerHTML = "Cost: " + availableItems[2].cost + " Bird(s)";
 app.append(cost3);
 
 let b2 = 0;
 const button2_count = document.createElement("h2");
-button2_count.innerHTML = "0.1 bird count: ";
+button2_count.innerHTML = availableItems[0].name + " Count: ";
 app.append(button2_count);
 
 let b3 = 0;
 const button3_count = document.createElement("h2");
-button3_count.innerHTML = "2.0 bird count: ";
+button3_count.innerHTML = availableItems[1].name + " Count: ";
 app.append(button3_count);
 
 let b4 = 0;
 const button4_count = document.createElement("h2");
-button4_count.innerHTML = "50 bird count: ";
+button4_count.innerHTML = availableItems[2].name + " Count: ";
 app.append(button4_count);
 
 const intervalID = setInterval(myCallback, 500);
@@ -77,32 +86,32 @@ button.addEventListener("click", function () {
 button2.addEventListener("click", function () {
   if (x >= 10) {
     x -= 10;
-    growth_rate += 0.1;
+    growth_rate += availableItems[0].rate;
     b2 += 1;
-    b2_price *= 1.15;
-    button2_count.innerHTML = "0.1 bird count: " + b2;
-    cost.innerHTML = "Cost: " + b2_price + " Birds";
+    availableItems[0].cost *= 1.15;
+    button2_count.innerHTML = availableItems[0].name + " Count: " + b2;
+    cost.innerHTML = "Cost: " + availableItems[0].cost + " Birds";
   }
 });
 
 button3.addEventListener("click", function () {
   if (x >= 100) {
     x -= 100;
-    growth_rate += 2.0;
+    growth_rate += availableItems[1].rate;
     b3 += 1;
-    b3_price *= 1.15;
-    button3_count.innerHTML = "2.0 bird count: " + b3;
-    cost2.innerHTML = "Cost: " + b3_price + " Birds";
+    availableItems[1].cost *= 1.15;
+    button3_count.innerHTML = availableItems[1].name + " Count: " + b3;
+    cost2.innerHTML = "Cost: " + availableItems[1].cost + " Birds";
   }
 });
 
 button4.addEventListener("click", function () {
   if (x >= 1000) {
     x -= 1000;
-    growth_rate += 50;
+    growth_rate += availableItems[2].rate;
     b4 += 1;
-    b4_price *= 1.15;
-    button4_count.innerHTML = "50 bird count: " + b4;
-    cost3.innerHTML = "Cost: " + b4_price + " Birds";
+    availableItems[2].cost *= 1.15;
+    button4_count.innerHTML = availableItems[2].name + " Count: " + b4;
+    cost3.innerHTML = "Cost: " + availableItems[2].cost + " Birds";
   }
 });
