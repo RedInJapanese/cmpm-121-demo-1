2a23dd0bd84e436445d83d71bfec09639c0bf53b
RedInJapanese <abasu924@gmail.com>
2024-10-07 11:13:06 -0700

    complete step 5

--- src/main.ts
+++ src/main.ts
@@ -14,35 +14,38 @@ button.innerHTML = "test 🅱️";
 app.append(button);
 
 let x = 0;
-let lastTime: number = 0;
-let frames: number = 0;
+let growth_rate = 0;
 
 const count = document.createElement("h2");
 count.innerHTML = "Cookie count: ";
 app.append(count);
+
+const button2 = document.createElement("button");
+button2.innerHTML = "Buy";
+app.append(button2);
+
+const cost = document.createElement("h2");
+cost.innerHTML = "Cost: 10 units";
+app.append(cost);
+
+
+const intervalID = setInterval(myCallback, 500);
+console.log(intervalID);
+function myCallback() {
+  x += growth_rate;
+  count.innerHTML = "Cookie count: " + x;
+}
 
 button.addEventListener("click", function () {
   x += 1;
   count.innerHTML = "Cookie count: " + x;
 });
 
-function fps(timestamp: number) {
-    if (lastTime) {
-      // Calculate the time difference between frames in milliseconds
-      const deltaTime = timestamp - lastTime;
-  
-      // Calculate FPS
-      frames = 1000 / deltaTime;
+button2.addEventListener("click", function () {
+    if (x >=10){
+        x -= 10;
+        growth_rate += 1;
     }
-    
-    lastTime = timestamp;  // Update the last time with the current timestamp
-    
-    if(frames > 0){
-        x += 1 / frames;
-        count.innerHTML = "Cookie count: " + x;
-    }
-    // Request the next animation frame
-    requestAnimationFrame(fps);
-  }
+});
 
-  requestAnimationFrame(fps);
+
