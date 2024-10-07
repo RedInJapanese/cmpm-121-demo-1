3467947a104f1e57dcd0df5976a5acaf58d03830
RedInJapanese <abasu924@gmail.com>
2024-10-07 14:18:40 -0700

    complete step 7

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ app.append(button);
 
 let x = 0;
 let growth_rate = 0;
+let b2_price = 10; 
+let b3_price = 100;
+let b4_price = 1000;
 
 const count = document.createElement("h2");
 count.innerHTML = "Cookie count: ";
@@ -44,17 +47,17 @@ const cost3 = document.createElement("h2");
 cost3.innerHTML = "Cost: 1000 units";
 app.append(cost3);
 
-let b2 = 0
+let b2 = 0;
 const button2_count = document.createElement("h2");
 button2_count.innerHTML = "0.1 count: ";
 app.append(button2_count);
 
-let b3 = 0
+let b3 = 0;
 const button3_count = document.createElement("h2");
 button3_count.innerHTML = "2.0 count: ";
 app.append(button3_count);
 
-let b4 = 0
+let b4 = 0;
 const button4_count = document.createElement("h2");
 button4_count.innerHTML = "50 count: ";
 app.append(button4_count);
@@ -75,25 +78,31 @@ button2.addEventListener("click", function () {
   if (x >= 10) {
     x -= 10;
     growth_rate += 0.1;
-    b2+=1
+    b2 += 1;
+    b2_price *= 1.15;
     button2_count.innerHTML = "0.1 count: " + b2;
+    cost.innerHTML = "Cost: "+ b2_price +" units";
   }
 });
 
 button3.addEventListener("click", function () {
-    if (x >= 100) {
-      x -= 100;
-      growth_rate += 2.0;
-      b3+=1
-      button3_count.innerHTML = "2.0 count: " + b3;
-    }
+  if (x >= 100) {
+    x -= 100;
+    growth_rate += 2.0;
+    b3 += 1;
+    b3_price *= 1.15;
+    button3_count.innerHTML = "2.0 count: " + b3;
+    cost2.innerHTML = "Cost: "+ b3_price +" units";
+}
 });
 
 button4.addEventListener("click", function () {
-    if (x >= 1000) {
-      x -= 1000;
-      growth_rate += 50;
-      b4+=1
-      button4_count.innerHTML = "50 count: " + b4;
-    }
+  if (x >= 1000) {
+    x -= 1000;
+    growth_rate += 50;
+    b4 += 1;
+    b4_price *= 1.15;
+    button4_count.innerHTML = "50 count: " + b4;
+    cost3.innerHTML = "Cost: "+ b4_price +" units";
+  }
 });
